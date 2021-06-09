6393bc3caee0989a77cd6dff69367f453ed2e1b8
danny <bandofcs@gmail.com>
2021-06-09 11:55:29 +0800

    Completed project

--- budget.py
+++ budget.py
@@ -1,4 +1,4 @@
-import re
+import math
 DEBUG=False
 
 class Category:
@@ -27,7 +27,7 @@ class Category:
             amount=(str(self.ledger[i])).split(",")[0].split(" ")[1]
             if amount.count(".")==0:
                 amount=amount+".00"
-            description=(str(self.ledger[i])).split(",")[1].split(": '")[1].rstrip("'}")
+            description=(str(self.ledger[i])).lstrip(", ").split(": '")[1].rstrip("'}")
             j=0
             while j<23:
                 if j<len(description):
@@ -60,7 +60,8 @@ class Category:
             if DEBUG:
                 print(amount)
             return True
-        else: return False
+        else: 
+            return False
 
     def get_balance(self):
         return self.balance
@@ -76,14 +77,70 @@ class Category:
         else: return False
 
     def check_funds(self, amount):
-        if int(amount)>int(self.get_balance()):
+        if amount>self.get_balance():
             return False
         else: return True
 
 def create_spend_chart(listofcategories):
+    percentagechart = ["","","","","","","","","","",""]
+
     total=0.00
     i=0
-    for i in range(int(len(listofcategories))):
+    percentagelist=list()
+    longestcategory=0
+    label=""
+    for i in range(len(listofcategories)):
         total=total+listofcategories[i].spent
-        print(format(total,'.2f'))
-    return 
+    i=0
+    for i in range(len(listofcategories)):
+        percentagelist.append(int(math.floor(listofcategories[i].spent/total*10.0))*10)
+        if len(listofcategories[i].categories)>longestcategory:
+            longestcategory=len(listofcategories[i].categories)
+
+    #Popular the bar chart
+    i=0
+    percentage=100
+    for i in range(len(percentagechart)):
+        j=0
+        for j in range(len(listofcategories)):
+            if percentage<=percentagelist[j]:
+                percentagechart[i]=percentagechart[i]+"o  "
+            else:
+                percentagechart[i]=percentagechart[i]+"   "
+        percentage=percentage-10
+    #print the bar chart
+    barchart="100| "+percentagechart[0]+"\n"\
+    +" 90| "+percentagechart[1]+"\n"\
+    +" 80| "+percentagechart[2]+"\n"\
+    +" 70| "+percentagechart[3]+"\n"\
+    +" 60| "+percentagechart[4]+"\n"\
+    +" 50| "+percentagechart[5]+"\n"\
+    +" 40| "+percentagechart[6]+"\n"\
+    +" 30| "+percentagechart[7]+"\n"\
+    +" 20| "+percentagechart[8]+"\n"\
+    +" 10| "+percentagechart[9]+"\n"\
+    +"  0| "+percentagechart[10]+"\n"
+
+    #print the line
+    j=0
+    line="    "
+    for j in range(len(listofcategories)):
+        line=line+"---"
+    line=line+"-\n"
+    #print the label
+    i=0
+    j=0
+    #for each row
+    for i in range(longestcategory):
+        if i>0:
+            label=label+"\n"
+        label=label+"     "
+        #for each category
+        for j in range(len(listofcategories)):
+            #for each letter in category
+            if i<len(listofcategories[j].categories):
+                label=label+listofcategories[j].categories[i]+"  "
+            else:
+                label=label+"   "
+        
+    return "Percentage spent by category\n"+barchart+line+label
